3f1350be27b42ef1865ec4c02be71875d1322aa5
madharla2024 <146385717+madharla2024@users.noreply.github.com>
2023-10-26 13:30:34 -0400

    tried to code characters

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,11 +10,13 @@ public class PlayerMove : MonoBehaviour
     public Transform GroundCheck;
     public LayerMask groundLayer;
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         
         rb = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
 
     }
 
@@ -37,6 +39,22 @@ public class PlayerMove : MonoBehaviour
             movementVector.y = jumpForce;
 
         }
+
+        //flip the player for movement
+        if(movementVector.x < 0) 
+        {
+
+            this.gameObject.transform.localScale = new Vector3(-.3f, .45f, .6f);
+
+        }
+        else
+        {
+
+            this.gameObject.transform.localScale = new Vector3(.3f, .45f, .6f);
+
+        }
+
+        animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
 
         rb.AddForce(movementVector);
 
