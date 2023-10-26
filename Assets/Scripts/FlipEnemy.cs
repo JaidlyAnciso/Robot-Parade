d7cebab5adb1202de4838d49a049d6cd9f7371fb
ChaseMcIntosh <chamcint@iu.edu>
2023-10-26 17:04:51 -0400

    Fixed enemy

--- Assets/Scripts/FlipEnemy.cs
+++ Assets/Scripts/FlipEnemy.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class FlipEnemy : MonoBehaviour
 {
+
+    void Start()
+    {
+        print("OK");
+    }
     
     private void OnTriggerEnter2D(Collider2D enemy)
     {
-        if(enemy.tag == "Enemy")
+        print("hello world");
+        if (enemy.tag == "Enemy")
+
         {
-            print("hello world");
+           
             enemy.transform.Rotate(0f, 180f, 0f);
         }
     }
