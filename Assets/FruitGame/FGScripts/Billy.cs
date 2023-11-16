c75567d0a08c9746452538300ba9e0581216fbba
ChaseMcIntosh <chamcint@iu.edu>
2023-11-16 16:04:54 -0500

    joishgosdhgishgshdgsjgshgoisdhg

---
+++ Assets/FruitGame/FGScripts/Billy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private Animator animator;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementVector = new Vector2();
+
+        bool grounded = Physics2D.OverlapCircle(groundCheck.position, 2f, groundLayer) != null;
+
+    
+        if (movementVector.x < 0)
+        {
+            this.gameObject.transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else
+        {
+            this.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
+
+
+        rb.AddForce(movementVector);
+    }
+}
