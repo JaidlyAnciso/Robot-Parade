2d7564f6c99f846c99e2107142a03ee23b394b25
ChaseMcIntosh <chamcint@iu.edu>
2023-11-16 16:44:23 -0500

    movement

--- Assets/FruitGame/FGScripts/BillyMovement.cs
+++ Assets/FruitGame/FGScripts/BillyMovement.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class BillyMovement : MonoBehaviour
 {
     public float speed;
+    public Transform groundCheck;
     public LayerMask groundLayer;
     private Rigidbody2D rb;
-    private Animator animator;
+    
+    // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        animator = this.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -21,7 +23,9 @@ public class PlayerMovement : MonoBehaviour
 
         bool grounded = Physics2D.OverlapCircle(groundCheck.position, 2f, groundLayer) != null;
 
-    
+        movementVector.x = Input.GetAxis("Horizontal") * speed;
+
+        
         if (movementVector.x < 0)
         {
             this.gameObject.transform.localScale = new Vector3(-1f, 1f, 1f);
@@ -31,7 +35,7 @@ public class PlayerMovement : MonoBehaviour
             this.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
         }
 
-        animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
+       
 
 
         rb.AddForce(movementVector);
